a155f63375eeb24722e74fb5134d729f9a26774c
Ekaterina Saukova <esaukova@outlook.com>
2023-07-05 17:32:33 +0300

    Statistic Service

---
+++ src/main/java/ru/netology/esaukova/hw7/services/StatisticsService.java
@@ -0,0 +1,12 @@
+package ru.netology.esaukova.hw7.services;
+public class StatisticsService {
+    public long findMax(long[] incomes) {
+        long currentMax = incomes[0];
+        for (long income : incomes) {
+            if (currentMax < income) {
+                currentMax = income;
+            }
+        }
+        return currentMax;
+    }
+}
